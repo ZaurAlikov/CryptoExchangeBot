275d76d30c16c9e470ba895be58dcab58e5c3ea2
z.alikov <Pu*7sRq2>
2018-05-29 22:34:59 +0300

    Добавил getAllTriangle

--- src/main/java/ru/algotrade/service/ExchangeService.java
+++ src/main/java/ru/algotrade/service/ExchangeService.java
@@ -1,5 +1,10 @@
 package ru.algotrade.service;
 
+import ru.algotrade.model.PairTriangle;
+
+import java.util.List;
+
 public interface ExchangeService {
     void startTrade();
+    List<PairTriangle> getAllTriangles(List<String> Pairs, String mainCur);
 }
--- src/main/java/ru/algotrade/service/impl/ExchangeServiceImpl.java
+++ src/main/java/ru/algotrade/service/impl/ExchangeServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.algotrade.model.PairTriangle;
 import ru.algotrade.model.TradePair;
 import ru.algotrade.service.ExchangeService;
 import ru.algotrade.service.TradeOperation;
@@ -11,6 +12,7 @@ import ru.algotrade.service.TradeOperation;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 @Service
@@ -28,13 +30,45 @@ public class ExchangeServiceImpl implements ExchangeService {
         List<String> allPairs = tradeOperation.getAllPair();
         List<String> allCoins = tradeOperation.getAllCoins();
         Map<String, BigDecimal> allPrices = tradeOperation.getAllPrices();
-
-        List<String> firstPairs  = new ArrayList<>();
-        for (String pair : allPairs){
-            if(pair.contains("USDT")){
-                firstPairs.add(pair);
-            }
-        }
+        List<PairTriangle> triangles = getAllTriangles(allPairs, "USDT");
         System.out.println(allPairs.get(0));
     }
+
+    @Override
+    public List<PairTriangle> getAllTriangles(List<String> Pairs, String mainCur){
+        List<String> firstPairs = new ArrayList<>();
+        List<String> coins = new ArrayList<>();
+        for (String pair : Pairs){
+            if(pair.contains(mainCur)){
+                firstPairs.add(pair);
+                coins.add(pair.replace(mainCur,""));
+            }
+        }
+        List<String> secondPairs  = new ArrayList<>();
+        for (String coin : coins){
+            for (String coin2 : coins){
+                if(!coin.equals(coin2)){
+                    secondPairs.add(coin.concat(coin2));
+                }
+            }
+        }
+        secondPairs.removeIf(s -> !Pairs.contains(s));
+        PairTriangle triangle;
+        List<PairTriangle> triangles = new ArrayList<>();
+        for (String pair1 : firstPairs){
+            for (String pair2 : secondPairs){
+                if(pair2.contains(pair1.replace(mainCur,""))){
+                    for (String pair3 : firstPairs){
+                        if(pair3.contains(pair2.replace(pair1.replace(mainCur,""),""))){
+                            triangle = new PairTriangle(pair1, pair2, pair3, true);
+                            triangles.add(triangle);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return triangles;
+    }
+
 }
