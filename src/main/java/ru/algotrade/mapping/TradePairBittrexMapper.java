b526c320ea531d25955e8c35047422944c04806f
zalikov <alikov.zaur@gmail.com>
2018-06-27 11:11:36 +0300

    Небольшие правки

--- src/main/java/ru/algotrade/mapping/TradePairBittrexMapper.java
+++ src/main/java/ru/algotrade/mapping/TradePairBittrexMapper.java
@@ -1,10 +1,33 @@
 package ru.algotrade.mapping;
 
+import de.elbatya.cryptocoins.bittrexclient.api.model.publicapi.Market;
+import de.elbatya.cryptocoins.bittrexclient.api.model.publicapi.MarketSummary;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import ru.algotrade.model.TradePair;
 
 @Mapper(componentModel = "spring")
 public interface TradePairBittrexMapper {
-
-    TradePair toTradePair();
+    @Mappings({
+            @Mapping(target = "symbol", source = "market.marketName"),
+            @Mapping(target = "baseAsset", source = "market.baseCurrency"),
+            @Mapping(target = "quoteAsset", source = "market.marketCurrency"),
+            @Mapping(target = "askPrice", source = "marketSummary.ask"),
+            @Mapping(target = "askQty", source = "marketSummary.baseVolume"),
+            @Mapping(target = "bidPrice", source = "marketSummary.bid"),
+            @Mapping(target = "bidQty", source = "marketSummary.volume"),
+            @Mapping(target = "marketPrice", source = "marketSummary.last"),
+            @Mapping(target = "tradeLimits", expression = "java(new ru.algotrade.model.TradeLimits(" +
+                    "market.marketName," +
+                    "symbolInfo.getStatus().name()," +
+                    "symbolInfo.getFilters().get(0).getMinPrice()," +
+                    "market.minTradeSize," +
+                    "symbolInfo.getFilters().get(0).getTickSize()," +
+                    "symbolInfo.getFilters().get(1).getMinQty()," +
+                    "symbolInfo.getFilters().get(1).getMaxQty()," +
+                    "symbolInfo.getFilters().get(1).getStepSize()," +
+                    "symbolInfo.getFilters().get(2).getMinNotional()))")
+    })
+    TradePair toTradePair(Market market, MarketSummary marketSummary);
 }
